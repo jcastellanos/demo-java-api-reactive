b721174695ac9e5b473ad4429da0ae16f588f609
Juan Castellanos <juancastellanosm@gmail.com>
2024-02-07 21:32:59 -0500

    Service

--- src/main/java/com/jcastellanos/demo/controller/MainController.java
+++ src/main/java/com/jcastellanos/demo/controller/MainController.java
@@ -1,30 +1,20 @@
 package com.jcastellanos.demo.controller;
 
+import com.jcastellanos.demo.model.City;
 import com.jcastellanos.demo.service.CityService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.result.view.Rendering;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-@Controller
+@RestController
 public class MainController {
     private CityService cityService;
 
     public MainController(CityService cityService) {
         this.cityService = cityService;
     }
-    @GetMapping("/")
-    public Mono<Rendering> home() {
-        var cities = cityService.getCities();
-        return Mono.just(Rendering.view("index")
-                .modelAttribute("cities", cities).build());
-    }
-
-    @GetMapping("/home")
-    public String home2(final Model model) {
-        var cities = cityService.getCities();
-        model.addAttribute("cities", cities);
-        return "index";
+    @GetMapping("/cities")
+    public Flux<City> home() {
+        return cityService.getCities();
     }
 }
