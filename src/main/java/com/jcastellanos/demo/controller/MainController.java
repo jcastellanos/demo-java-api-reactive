fb8e674bf983a8cc48ddcbe5c484250019b17b50
Juan Castellanos <juancastellanosm@gmail.com>
2024-08-30 08:18:47 -0500

    Adicion variable de entorno para delimitar el tamano

--- src/main/java/com/jcastellanos/demo/controller/MainController.java
+++ src/main/java/com/jcastellanos/demo/controller/MainController.java
@@ -2,19 +2,32 @@ package com.jcastellanos.demo.controller;
 
 import com.jcastellanos.demo.model.City;
 import com.jcastellanos.demo.service.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
 public class MainController {
-    private CityService cityService;
 
-    public MainController(CityService cityService) {
-        this.cityService = cityService;
-    }
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+    @Autowired
+    private CityService cityService;
+    @Value("${city.limit}")
+    private String cityLimit;
+
     @GetMapping("/cities")
     public Flux<City> home() {
-        return cityService.getCities();
+        var limit = 21;
+        try {
+            limit = Integer.parseInt(cityLimit);
+        } catch (NumberFormatException ex) {
+            // No es necesario relizar nada
+            log.warn("Asignando valor por defecto a city.limit");
+        }
+        return cityService.getCities().take(limit);
     }
 }
--- src/main/java/com/jcastellanos/demo/service/CityService.java
+++ src/main/java/com/jcastellanos/demo/service/CityService.java
@@ -10,9 +10,28 @@ import java.util.Arrays;
 public class CityService {
 
     public Flux<City> getCities() {
-        return Flux.fromIterable(Arrays.asList(new City[]{new City("Bogota", "Colombia"),
+        return Flux.fromIterable(Arrays.asList(new City[]{
+                new City("Bogota", "Colombia"),
                 new City("Cali", "Colombia"),
                 new City("Medellin", "Colombia"),
-                new City("Cartagena", "Colombia")}));
+                new City("Cartagena", "Colombia"),
+                new City("Barranquilla", "Colombia"),
+                new City("Cucuta", "Colombia"),
+                new City("Bucaramanga", "Colombia"),
+                new City("Valledupar", "Colombia"),
+                new City("Villavicencio", "Colombia"),
+                new City("Pereira", "Colombia"),
+                new City("Manizales", "Colombia"),
+                new City("Pasto", "Colombia"),
+                new City("Neiva", "Colombia"),
+                new City("Nueva York", "Estados Unidos"),
+                new City("Baltimore", "Estados Unidos"),
+                new City("Detroit", "Estados Unidos"),
+                new City("Austin", "Estados Unidos"),
+                new City("Oklahoma", "Estados Unidos"),
+                new City("Reno", "Estados Unidos"),
+                new City("Louisville", "Estados Unidos"),
+                new City("Kansas", "Estados Unidos"),
+        }));
     }
 }
